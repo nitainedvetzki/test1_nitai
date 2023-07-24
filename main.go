b2d91a1fa9e0e014c19cc8867b301076ca93cd9b
nitainedvetzki <106998867+nitainedvetzki@users.noreply.github.com>
2023-07-24 19:26:05 +0300

    Add files via upload

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/server"
+)
+
+func main() {
+	fmt.Println("Hello, Cosmos SDK!")
+	
+	// Add your code using cosmos-sdk packages here.
+	// For example, you can create and start a new ABCI application.
+}
+
